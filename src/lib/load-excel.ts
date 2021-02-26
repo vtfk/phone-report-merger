c35113ffeca2b5fa91fcecf9dfbeafbae8f1e0b6
Ingar Helgesen <ingar@i-h.no>
2021-02-26 13:29:54 +0100

    Added automatic header locator

--- src/lib/load-excel.ts
+++ src/lib/load-excel.ts
@@ -3,17 +3,27 @@ import Excel, { RowModel } from 'exceljs'
 export async function loadExcel (path: string, headerRow: number = 1): Promise<any[]> {
   const workbook = new Excel.Workbook()
   await workbook.xlsx.readFile(path)
+  const worksheet = workbook.worksheets[0]
 
   // Missing property in declaration file
   /* @ts-expect-error */
-  const rows = workbook.worksheets[0].model.rows as RowModel[]
-  const headerRowIndex = headerRow - 1
-  const header = rows[headerRowIndex].cells.map(cell => cell.value?.toString().trim()) as string[]
+  const rows = worksheet.model.rows as RowModel[]
 
+  let headerRowIndex: number
+  let header: string[]
   const sheet: any[] = []
   rows.forEach((row, y) => {
-    const colIndex = y - headerRowIndex - 1
-    if (y <= headerRowIndex) return
+    const rowColumnCount = row.cells.filter(cell => typeof cell.value !== 'undefined').length
+
+    if (headerRowIndex === undefined && rowColumnCount === worksheet.actualColumnCount) {
+      console.log(`Cells: ${row.cells.length} & Y: ${y} - Path: ${path}`)
+      headerRowIndex = y
+      const formatCell = (cell: any): string => cell.value?.toString().trim()
+      header = rows[headerRowIndex].cells.map(formatCell)
+    } else if (headerRowIndex === undefined) return
+
+    const colIndex = y - headerRowIndex
+
     sheet[colIndex] = {}
     row.cells.forEach((cell, x) => {
       sheet[colIndex][header[x]] = cell.value
