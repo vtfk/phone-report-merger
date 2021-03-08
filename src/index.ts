5a92a1bda20d2165d46662f42dfb7f9879a589e2
Ingar Helgesen <ingar@i-h.no>
2021-03-08 11:17:02 +0100

    Changed to @vtfk/logger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { logger } from '@vtfk/logger'
+import glob from 'fast-glob'
 import { PhoneInformation, TechstepRecord, TelenorReport } from './lib/types'
 import { saveExcel } from './lib/save-excel'
 import { getTechstepReport, getTelenorReport } from './lib/get-report'
 import { mkdirIfNotExists } from './lib/mkdir-if-not-exist'
-import glob from 'fast-glob'
 
 const outputReportPath = './data/device-report.xlsx'
 const paths = [
@@ -12,42 +13,43 @@ const paths = [
 
 ;(async () => {
   const startTime = new Date()
-  console.log('Start!')
+  logger('info', ['Start'])
+
   if (mkdirIfNotExists(paths)) {
-    console.log('Created missing directories..')
-    console.log(`Put all Techstep reports (.xlsx) in "${paths[0]}"`)
-    console.log(`And put all Telenor reports (.xlsx) in "${paths[1]}"`)
-    console.log('Then re-run the script.')
+    logger('warn', ['Created missing directories..'])
+    logger('warn', [`Please put all Techstep reports (.xlsx) in "${paths[0]}"`])
+    logger('warn', [`And put all Telenor reports (.xlsx) in "${paths[1]}"`])
+    logger('warn', ['Then re-run the script.'])
     process.exit(0)
   }
   const techstepReportPaths = await glob('./data/techstep/*.xlsx', { onlyFiles: true })
   const telenorReportPaths = await glob('./data/telenor/*.xlsx', { onlyFiles: true })
 
   if (techstepReportPaths.length < 1) {
-    console.log(`Couldn't find any (.xlsx) files in "${paths[0]}"`)
+    logger('error', [`Couldn't find any (.xlsx) files in "${paths[0]}"`])
   }
   if (telenorReportPaths.length < 1) {
-    console.log(`Couldn't find any (.xlsx) files in "${paths[1]}"`)
+    logger('error', [`Couldn't find any (.xlsx) files in "${paths[1]}"`])
   }
   if (techstepReportPaths.length < 1 && telenorReportPaths.length < 1) {
     process.exit(1)
   }
 
-  console.log('Importing reports...')
+  logger('info', ['Importing reports...'])
 
   const techstepReport: TechstepRecord[] = []
   await Promise.all(techstepReportPaths.map(async path => {
     techstepReport.push(...await getTechstepReport(path))
   }))
 
-  console.log(`Found ${techstepReportPaths.length} Techstep reports, with a total of ${techstepReport.length} records.`)
+  logger('info', [`Found ${techstepReportPaths.length} Techstep reports, with a total of ${techstepReport.length} records.`])
 
   const telenorReport: TelenorReport[] = []
   await Promise.all(telenorReportPaths.map(async path => {
     telenorReport.push(...await getTelenorReport(path))
   }))
 
-  console.log(`Found ${telenorReportPaths.length} Telenor reports, with a total of ${telenorReport.length} records.`)
+  logger('info', [`Found ${telenorReportPaths.length} Telenor reports, with a total of ${telenorReport.length} records.`])
 
   const userDeviceReport: PhoneInformation[] = []
   telenorReport.forEach(record => {
@@ -59,11 +61,11 @@ const paths = [
     })
   })
 
-  console.log(`Filled ${userDeviceReport.length}/${telenorReport.length} records`)
+  logger('info', [`Filled ${userDeviceReport.length}/${telenorReport.length} records`])
 
   await saveExcel(outputReportPath, userDeviceReport)
-  console.log(`Saved report to ${outputReportPath}`)
+  logger('info', [`Saved report to ${outputReportPath}`])
 
   const timeUsed = (new Date()).getTime() - startTime.getTime()
-  console.log(`Done! Time used ${timeUsed}ms`)
+  logger('info', ['Done', 'Time used', `${timeUsed}ms`])
 })().catch(console.error)
